d69a98199aa5fdee94be62ab212a6c59cdc81c53
fasd0g <1fasdog1@gmail.com>
2026-02-06 08:33:16 +0300

    Add files via upload

---
+++ src/main/java/com/example/aichat/AiChatPlugin.java
@@ -0,0 +1,11 @@
+
+package com.example.aichat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AiChatPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("AiChat enabled.");
+    }
+}
